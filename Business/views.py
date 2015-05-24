99d106f0899f68298a56d00269cb89b736094313
Michael Rhyndress <rhynd1ml@cmich.edu>
2015-05-24 15:10:57 -0400

    set up business views, urls, and templates

--- Business/views.py
+++ Business/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_list_or_404, get_object_or_404, redirect, render, render_to_response
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.generic import View
+from django.utils.timezone import now
 
 # Create your views here.
+class BusinessHomepage(View):
+    """
+    Homepage for Business Users
+    """
+    def post(self, request, *args, **kwargs):
+        pass
+    
+    def get(self, request, *args, **kwargs):
+        #if request.user.is_authenticated():
+            # go to business view
+        return render(request, 'business.html')
+    
